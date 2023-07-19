c09bc584f6ab0c71fb87a1c4c20c3d33a00af5d9
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-07-18 21:09:25 -0300

    Refactor flash msg for ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -9,9 +9,11 @@ class IngredientsController < ApplicationController
 
   def create
     @ingredient = Ingredient.create(params.require(:ingredient).permit(:name_ingredient, :attr_1, :attr_2, :attr_3,:exparation_date, :last_price))
-    if @ingredient
+    if @ingredient == true
+      # Inventory.create!(ingredient_id: @ingredient.id, quantity: 1)
       redirect_to ingredient_path(@ingredient)
     else
+      flash[:notice] = "There's been an error."
       render 'new'
     end
   end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,5 @@
 class Ingredient < ApplicationRecord
   has_one :inventory
+
+  validates :name_ingredient, presence:, uniqueness: true
 end
