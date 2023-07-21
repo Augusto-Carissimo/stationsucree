3941a73f40f330bfc99a57f13c017a9d5f71c807
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-07-21 12:25:53 -0300

    Replace StockPerLocation#index with Location#index, add Location controller, route, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'locations/index'
-  get 'locations/show'
   root "pages#home"
   resources :ingredients, only: [:show, :new, :create]
   resources :inventories, only: [:index, :edit, :update]
