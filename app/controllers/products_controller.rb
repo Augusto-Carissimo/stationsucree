c4909bf3c4912fe245bd3533499a96875b5aff23
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-07-23 20:51:13 -0300

    Add Product#index and Product#show

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,24 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(quantity: @product.quantity + params.require(:product).permit(:quantity)[:quantity].to_i)
+      flash[:notice] = 'Product updated'
+      redirect_to products_path
+    else
+      flash[:notice] = "There's been an error."
+      redirect_to products_path
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   root "pages#home"
   resources :ingredients, only: [:show, :new, :create]
   resources :inventories, only: [:index, :edit, :update]
   resources :stock_per_locations, only: [:edit, :update]
   resources :locations, only: [:show, :index]
+  resources :products, only: [:index, :show, :edit, :update]
 end
