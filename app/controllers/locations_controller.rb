f27683337247e4a9dd72d824a10b9e97ed35b9bb
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-07-25 16:27:49 -0300

    Add Location.create

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,4 +6,19 @@ class LocationsController < ApplicationController
   def show
     @location = Location.find(params[:id])
   end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.create(params.require(:location).permit(
+      :name_location, :address, :phone))
+    if @location.save
+      redirect_to location_path(@location)
+    else
+      flash[:notice] = "There's been an error."
+      render 'new'
+    end
+  end
 end
