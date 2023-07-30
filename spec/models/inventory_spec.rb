c4c338a99a25f59218c6a63c90940d1ab63640fe
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-07-29 22:28:51 -0300

    Add inventory_spec

---
+++ spec/models/inventory_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Inventory do
+  describe 'associations' do
+    it { is_expected.to belong_to(:ingredient) }
+  end
+
+  describe 'validation' do
+    it { is_expected.to validate_numericality_of(:quantity).is_greater_than_or_equal_to(0) }
+  end
+end
