d9e96f881ba341aa04e4dbe0da254a290562d980
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-08-02 19:22:11 -0300

    Add Sold action option to StockPerLocation#update

--- app/controllers/stock_per_locations_controller.rb
+++ app/controllers/stock_per_locations_controller.rb
@@ -6,16 +6,27 @@ class StockPerLocationsController < ApplicationController
 
   def update
     @stock = StockPerLocation.find(params[:id])
-    stock_params =
-    params.require(:stock_per_location).permit(:quantity_stock)[:quantity_stock].to_i
-    if @stock.update(quantity_stock: @stock.quantity_stock + stock_params)
-      flash[:notice] = 'Stock updated'
-      product = Product.find(@stock.product.id)
-      product.update(quantity_product: product.quantity_product - stock_params)
-      redirect_to locations_path
+    p '-'*50
+    if params[:commit] == 'Sold'
+      stock_params = params.require(:stock_per_location).permit(:quantity_stock)[:quantity_stock].to_i
+      if @stock.update(quantity_stock: @stock.quantity_stock - stock_params)
+        flash[:notice] = 'Stock updated'
+        redirect_to locations_path
+      else
+        flash[:notice] = "There's been an error"
+        redirect_to locations_path
+      end
     else
-      flash[:notice] = "There's been an error"
-      redirect_to locations_path
+      stock_params = params.require(:stock_per_location).permit(:quantity_stock)[:quantity_stock].to_i
+      if @stock.update(quantity_stock: @stock.quantity_stock + stock_params)
+        flash[:notice] = 'Stock updated'
+        product = Product.find(@stock.product.id)
+        product.update(quantity_product: product.quantity_product - stock_params)
+        redirect_to locations_path
+      else
+        flash[:notice] = "There's been an error"
+        redirect_to locations_path
+      end
     end
   end
 end
