12a7bd6f7f61e4e4367ecb261b1e5b2fb243bf70
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-08-03 08:32:12 -0300

    Refactor StockPerLocation#update

--- app/controllers/stock_per_locations_controller.rb
+++ app/controllers/stock_per_locations_controller.rb
@@ -6,27 +6,38 @@ class StockPerLocationsController < ApplicationController
 
   def update
     @stock = StockPerLocation.find(params[:id])
-    p '-'*50
     if params[:commit] == 'Sold'
-      stock_params = params.require(:stock_per_location).permit(:quantity_stock)[:quantity_stock].to_i
-      if @stock.update(quantity_stock: @stock.quantity_stock - stock_params)
-        flash[:notice] = 'Stock updated'
-        redirect_to locations_path
-      else
-        flash[:notice] = "There's been an error"
-        redirect_to locations_path
-      end
+      sold
     else
-      stock_params = params.require(:stock_per_location).permit(:quantity_stock)[:quantity_stock].to_i
-      if @stock.update(quantity_stock: @stock.quantity_stock + stock_params)
-        flash[:notice] = 'Stock updated'
-        product = Product.find(@stock.product.id)
-        product.update(quantity_product: product.quantity_product - stock_params)
-        redirect_to locations_path
-      else
-        flash[:notice] = "There's been an error"
-        redirect_to locations_path
-      end
+      transfer
+    end
+  end
+
+  private
+
+  def stock_update_params
+    params.require(:stock_per_location).permit(:quantity_stock)[:quantity_stock].to_i
+  end
+
+  def sold
+    if @stock.update(quantity_stock: @stock.quantity_stock - stock_update_params)
+      flash[:notice] = 'Stock updated'
+      redirect_to locations_path
+    else
+      flash[:notice] = "There's been an error"
+      redirect_to locations_path
+    end
+  end
+
+  def transfer
+    if @stock.update(quantity_stock: @stock.quantity_stock + stock_update_params)
+      flash[:notice] = 'Stock updated'
+      product = Product.find(@stock.product.id)
+      product.update(quantity_product: product.quantity_product - stock_update_params)
+      redirect_to locations_path
+    else
+      flash[:notice] = "There's been an error"
+      redirect_to locations_path
     end
   end
 end
