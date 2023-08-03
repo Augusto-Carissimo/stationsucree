60b4a0473642d9bd23843f1d7550d20c9fc4dd43
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-08-03 09:28:44 -0300

    Refactor StockPerLocation#update

--- app/controllers/stock_per_locations_controller.rb
+++ app/controllers/stock_per_locations_controller.rb
@@ -7,9 +7,9 @@ class StockPerLocationsController < ApplicationController
   def update
     @stock = StockPerLocation.find(params[:id])
     if params[:commit] == 'Sold'
-      sold
+      sold_product
     else
-      transfer
+      transfer_from_lab
     end
   end
 
@@ -19,25 +19,36 @@ class StockPerLocationsController < ApplicationController
     params.require(:stock_per_location).permit(:quantity_stock)[:quantity_stock].to_i
   end
 
-  def sold
+  def sold_product
     if @stock.update(quantity_stock: @stock.quantity_stock - stock_update_params)
       flash[:notice] = 'Stock updated'
       redirect_to locations_path
     else
-      flash[:notice] = "There's been an error"
-      redirect_to locations_path
+      error_messagge
     end
   end
 
-  def transfer
-    if @stock.update(quantity_stock: @stock.quantity_stock + stock_update_params)
+  def transfer_from_lab
+    if check_product_quantity >= stock_update_params && @stock.update(quantity_stock: @stock.quantity_stock + stock_update_params)
       flash[:notice] = 'Stock updated'
-      product = Product.find(@stock.product.id)
-      product.update(quantity_product: product.quantity_product - stock_update_params)
+      subtract_from_lab
       redirect_to locations_path
     else
-      flash[:notice] = "There's been an error"
-      redirect_to locations_path
+      error_messagge
     end
+  end
+
+  def subtract_from_lab
+    product = Product.find(@stock.product.id)
+    product.update(quantity_product: product.quantity_product - stock_update_params)
+  end
+
+  def check_product_quantity
+    Product.find(@stock.product.id).quantity_product
+  end
+
+  def error_messagge
+    flash[:notice] = "There's been an error"
+    redirect_to locations_path
   end
 end
