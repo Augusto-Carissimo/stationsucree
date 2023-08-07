7d474d162bdafd9769d91bc804df695591c7a3bf
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-08-07 18:25:55 -0300

    Add inventories_controller_spec

---
+++ spec/requests/inventories_controller_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe InventoriesController, type: :request do
+  context do
+    let!(:inventory) { Inventory.create!(ingredient_id: Ingredient.create(name_ingredient: 'Flour').id) }
+
+    describe '#index' do
+      it 'index' do
+        get inventories_path
+        expect(response).to render_template(:index)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    describe '#update' do
+      it 'is updated' do
+        patch inventory_path(inventory), params: { inventory: { quantity_inventory: 10 } }
+        inventory.reload
+        expect(inventory.quantity_inventory).to eq(10)
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(inventories_path)
+      end
+
+      it 'fails' do
+        patch inventory_path(inventory), params: { inventory: { quantity_inventory: -1 } }
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(inventories_path)
+      end
+    end
+  end
+end
