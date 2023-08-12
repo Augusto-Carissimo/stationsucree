657a7ad365a021206e1b856d9788a473f04a59d2
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-08-12 13:07:04 -0300

    Refactor Ingredient schema and views

---
+++ db/migrate/20230812151235_remove_column_ingredient.rb
@@ -0,0 +1,8 @@
+class RemoveColumnIngredient < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :ingredients, :attr_1
+    remove_column :ingredients, :attr_2
+    remove_column :ingredients, :attr_3
+    remove_column :ingredients, :exparation_date
+  end
+end
---
+++ db/migrate/20230812151657_add_column_ingredient.rb
@@ -0,0 +1,7 @@
+class AddColumnIngredient < ActiveRecord::Migration[7.0]
+  def change
+    add_column :ingredients, :size, :float, default: 0.0
+    add_column :ingredients, :brand, :string
+    add_column :ingredients, :description, :string
+  end
+end
