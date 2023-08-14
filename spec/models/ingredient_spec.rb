2694ab7982ce1eb495f9b7e329005d28eec96a95
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-08-14 12:22:11 -0300

    Add validation Ingredient#last_price

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe Ingredient do
 
   describe 'validation' do
     it { is_expected.to validate_uniqueness_of(:name_ingredient) }
+    it { is_expected.to validate_numericality_of(:last_price).is_greater_than_or_equal_to(0) }
   end
 end
