0c336186f3b8e087084b9717ed903be6277e7292
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-08-15 16:38:33 -0300

    Add check_availability_ingredients and consume ingredients to Product#update and refactor Product.controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,35 +45,60 @@ class ProductsController < ApplicationController
 
   private
 
+  def quantity_params
+    params.require(:product).permit(:quantity_product)[:quantity_product].to_f
+  end
+
+  def product_with_recipe
+    @prod_join ||= Product.distinct(:id).where(id: @product.id).joins(recipe: :ingredient_recipes).first
+  end
+
   def update_name_and_recipe
     if @product.update(params.require(:product).permit(:name_product, :recipe_text))
-      flash[:notice] = 'Product updated'
-      redirect_to products_path
+      successful_message
     else
-      flash[:notice] = "There's been an error."
-      redirect_to products_path
+      error_message
     end
   end
 
   def update_quantity
-    check_availability_ingredients
-    # if @product.update(quantity_product: @product.quantity_product + params.require(:product).permit(:quantity_product)[:quantity_product].to_f)
-    #   flash[:notice] = 'Product updated'
-    #   redirect_to products_path
-    # else
-    #   flash[:notice] = "There's been an error."
-    #   redirect_to products_path
-    # end
+    if check_availability_ingredients.any?
+      flash[:notice] = "There's not enough #{check_availability_ingredients.each { |ing| ing } } "
+      redirect_to products_path
+    else
+      if @product.update(quantity_product: @product.quantity_product + quantity_params) && consume_ingredients
+        successful_message
+      else
+        error_message
+      end
+    end
   end
 
   def check_availability_ingredients
-    p '-'*50
-    p @product.recipe
-    p '-'*50
-
+    missing_ingredients = []
+    product_with_recipe.recipe.ingredient_recipes.each do |ingredient_recipe|
+      necessary_amount = ingredient_recipe.quantity_recipe * quantity_params
+      available_amount = ingredient_recipe.ingredient.inventory.quantity_inventory
+      if necessary_amount > available_amount
+        missing_ingredients << ingredient_recipe.ingredient.name_ingredient
+      end
+    end
+    missing_ingredients
   end
 
   def consume_ingredients
+    product_with_recipe.recipe.ingredient_recipes.each do |ingredient_recipe|
+      Inventory.find(ingredient_recipe.ingredient.inventory.id).update(quantity_inventory: ingredient_recipe.ingredient.inventory.quantity_inventory - (ingredient_recipe.quantity_recipe * quantity_params))
+    end
+  end
 
+  def successful_message
+    flash[:notice] = 'Product updated'
+    redirect_to products_path
+  end
+
+  def error_message
+    flash[:notice] = "There's been an error."
+    redirect_to products_path
   end
 end
