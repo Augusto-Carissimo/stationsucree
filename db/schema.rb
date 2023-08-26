cfa3eb50208e528e2628ca87eb71d98cf87b2186
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-08-26 14:10:17 -0300

    Add User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_14_151833) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_26_163348) do
   create_table "ingredient_recipes", force: :cascade do |t|
     t.integer "recipe_id", null: false
     t.integer "ingredient_id", null: false
@@ -87,6 +87,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_14_151833) do
     t.string "name"
     t.string "email"
     t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
