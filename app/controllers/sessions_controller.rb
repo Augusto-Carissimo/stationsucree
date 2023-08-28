f577afe391a478d3803a7448aef3f84ab5e1c630
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-08-28 08:53:57 -0300

    Add logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # p '-'*50
-    # p params[:session]
-    # p '-'*50
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
@@ -22,7 +19,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
-    redirect_to login_path
+    redirect_to root_path
   end
 
   private
