e996c66825e65b7064f3fd858cf3f1652286cd95
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-09-09 14:55:25 -0300

    Add IngredientRecipe#price_ingredient

--- app/models/ingredient_recipe.rb
+++ app/models/ingredient_recipe.rb
@@ -3,4 +3,8 @@ class IngredientRecipe < ApplicationRecord
   belongs_to :ingredient
 
   validates :quantity_recipe, numericality: { greater_than_or_equal_to: 0 }
+
+  def price_ingredient
+    @price_ingredient ||= (ingredient.last_price * quantity_recipe).round(2)
+  end
 end
