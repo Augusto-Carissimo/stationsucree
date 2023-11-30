ae7d29f00abdee39f114edbc5a6ac86d2ece1049
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-11-30 13:21:16 -0300

    Add PriceHistory to Ingredient#show

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -3,6 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient do
+  describe 'associations' do
+    it { is_expected.to have_many(:price_histories) }
+  end
+
   describe 'validation' do
     it { is_expected.to validate_numericality_of(:quantity_ingredient).is_greater_than_or_equal_to(0) }
     it { is_expected.to validate_uniqueness_of(:name_ingredient) }
