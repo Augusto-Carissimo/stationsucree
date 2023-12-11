87b4281201b67f27283afe07901c8726edbde5e0
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-12-11 00:25:37 -0300

    Add PriceHistory.brand PriceHistory.supplier

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_30_200241) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_11_025616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_200241) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "brand"
+    t.string "supplier"
     t.index ["ingredient_id"], name: "index_price_histories_on_ingredient_id"
   end
 
